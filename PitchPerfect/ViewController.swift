5d0b5c935f24cccc9a33979e0ddb8de81896b34b
Anilesh <30379145+cyrusMinati@users.noreply.github.com>
2017-11-28 20:43:55 +0530

    Adding buttons and images to assets

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var stoprecordingButton: UIButton!
+    @IBOutlet weak var recordingButton: UIButton!
+    @IBOutlet weak var recording_label: UILabel!
+    
     override func viewDidLoad() {
+        @IBOutlet weak var stoprecordingButton: UIButton!
+        @IBOutlet weak var stoprecordingButton: UIButton!
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        @IBOutlet weak var recordButton: UIButton!
+        print("viewWillAppear called")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        <#code#>
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func RecordAudio(_ sender: Any) {
+        print("Record Button was Pressed")
+        recording_label.text = "Recording in Progress"
+    }
+    @IBAction func stoprecording(_ sender: Any) {
+        print("Stop Button was Pressed")
+        recording_label.text = "Recording Completed"
+    }
 }
 
